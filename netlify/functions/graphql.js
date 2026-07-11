2c760f27763d605db6c1cc1950ddf2fa2dfb5b8f
danielkolawole730 <111348576+danielkolawole730@users.noreply.github.com>
2026-07-11 18:24:26 +0100

    Change GraphQL endpoint path to '/graphql'

--- netlify/functions/graphql.js
+++ netlify/functions/graphql.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const { ApolloServer } = require('apollo-server-express');
-
 const { typeDefs, resolvers } = require('../../server/schemas');
 const { authMiddleware } = require('../../server/utils/auth');
 require('../../server/config/connection');
-
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 let serverlessHandler;
-
 const getHandler = async () => {
   if (!serverlessHandler) {
     const server = new ApolloServer({
@@ -20,13 +16,13 @@ const getHandler = async () => {
       context: authMiddleware,
     });
     await server.start();
-    server.applyMiddleware({ app, path: '/.netlify/functions/graphql' });
+    server.applyMiddleware({ app, path: '/graphql' });
     serverlessHandler = serverless(app);
   }
   return serverlessHandler;
 };
-
 exports.handler = async (event, context) => {
   const handler = await getHandler();
+  event.path = '/graphql';
   return handler(event, context);
 };
